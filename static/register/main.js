6bc509d7d3324ff8c3576a586e53bc70e55be04d
Matias Barrios <GCabrera@humana.com>
2020-02-14 09:35:13 -0300

    master

--- static/register/main.js
+++ static/register/main.js
@@ -1,4 +1,5 @@
 let username = document.getElementsById('username')
+let email = document.getElementsById('email')
 let password = document.getElementsById('password')
 let password_check = document.getElementsById('password_check')
 
