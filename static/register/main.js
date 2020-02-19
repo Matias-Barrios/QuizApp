9a976bf84cd22488887a42e898ae6dd81165328b
Matias Barrios <GCabrera@humana.com>
2020-02-19 13:30:38 -0300

    master

--- static/register/main.js
+++ static/register/main.js
@@ -1,58 +1,103 @@
-let usernameInput = document.getElementById('Name');
-let emailInput = document.getElementById('Email');
-let passwordInput = document.getElementById('Pass');
-let password_checkInput = document.getElementById('PassRepeat');
-
-
 function ValidateUsername(textBox) {
-    if (__validateUserName(textBox.value))
+    let register_button = document.getElementById('register');
+
+    if (__validateUserName(textBox.value)){
         textBox.style.background = "#53bd66";
-    else
+        register_button.disabled = false
+    }
+    else{
         textBox.style.background = "#d1736d";
+        register_button.disabled = true;
+    }
 }
 
 function ValidateEmail(textBox) {
-    if (__validateEmail(textBox.value))
+    let register_button = document.getElementById('register');
+    if (__validateEmail(textBox.value)){
         textBox.style.background = "#53bd66";
-    else
+        delete register_button.disabled;
+    }
+    else{
         textBox.style.background = "#d1736d";
+        register_button.disabled = true;
+    }
 }
 
 function ValidatePassword(textBox) {
+    let register_button = document.getElementById('register');
     let passwordRules ={
         password_validation_length :  document.getElementById('password_validation_length'),
         password_validation_oneupper :  document.getElementById('password_validation_oneupper'),
         password_validation_onelower :  document.getElementById('password_validation_onelower'),
         password_validation_special :  document.getElementById('password_validation_special')
     }
-    if (__testPasswordisAtLeast8Characters(textBox.value))
+    if (__testPasswordisAtLeast8Characters(textBox.value)){
         passwordRules["password_validation_length"].style.color = "#53bd66";
-    else
+        delete register_button.disabled;
+    }
+    else{
         passwordRules["password_validation_length"].style.color = "#d1736d";
+        register_button.disabled = true;
+    }
     
-    if (__testPasswordisHasAtLeastOneLowerCaseCharacter(textBox.value))
+    if (__testPasswordisHasAtLeastOneLowerCaseCharacter(textBox.value)){ 
         passwordRules["password_validation_onelower"].style.color = "#53bd66";
-    else
+        delete register_button.disabled;
+    }
+    else { 
         passwordRules["password_validation_onelower"].style.color = "#d1736d";
+        register_button.disabled = true;
+    }
     
-    if (__testPasswordisHasAtLeastOneUpperCaseCharacter(textBox.value))
+    if (__testPasswordisHasAtLeastOneUpperCaseCharacter(textBox.value)){ 
         passwordRules["password_validation_oneupper"].style.color = "#53bd66";
-    else
+        delete register_button.disabled;
+    }
+    else{ 
         passwordRules["password_validation_oneupper"].style.color = "#d1736d";
+        register_button.disabled = true;
+    }
 
-    if (__testPasswordisHasAtLeastOneSpecialCharacter(textBox.value))
+    if (__testPasswordisHasAtLeastOneSpecialCharacter(textBox.value)){ 
         passwordRules["password_validation_special"].style.color = "#53bd66";
-    else
+        delete register_button.disabled;
+    }
+    else {
         passwordRules["password_validation_special"].style.color = "#d1736d";
+        register_button.disabled = true;
+    }
     
     let passwordInput = document.getElementById('Pass');
     if (__testPasswordisAtLeast8Characters(textBox.value) &&
         __testPasswordisHasAtLeastOneLowerCaseCharacter(textBox.value) &&
         __testPasswordisHasAtLeastOneUpperCaseCharacter(textBox.value) &&
-        __testPasswordisHasAtLeastOneSpecialCharacter(textBox.value))
+        __testPasswordisHasAtLeastOneSpecialCharacter(textBox.value)){ 
         passwordInput.style.background = "#53bd66";
-    else
+        delete register_button.disabled;    
+    }
+    else{
+        register_button.disabled = true
         passwordInput.style.background = "#d1736d";
+    }
+}
+
+function ValidateRepeat(textBox){
+    let register_button = document.getElementById('register');
+    if (textBox == null) {
+        register_button.disabled = true;
+        return;
+    }
+    if ( __validateRepeat(textBox.value) ){
+        delete register_button.disabled;
+    }else{
+        register_button.disabled = true;
+    }
+}
+
+function __validateRepeat(input){
+    let password_text = document.getElementById('Pass').value
+    let repeat_text = input
+    return (password_text != null && repeat_text != null ) && ( password_text == repeat_text )
 }
 
 function __validateEmail(input) {
@@ -71,9 +116,7 @@ function __testPasswordisAtLeast8Characters(input){
 }
 
 function __testPasswordisHasAtLeastOneLowerCaseCharacter(input){
-    console.log("Adentro : "+ input);
     var re = /^(?=.*[a-z]).*$/;
-    console.log(re.test(String(input)))
     return re.test(String(input));
 }
 
