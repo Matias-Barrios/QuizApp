cd06aaa02c022a77180dc74a1b5bf662d5ea9701
Matias Barrios <GCabrera@humana.com>
2020-02-24 10:20:59 -0300

    master

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Matias-Barrios/QuizApp/config"
 	"github.com/Matias-Barrios/QuizApp/router"
 )
 
 func main() {
+	f, err := os.OpenFile("/var/log/quizapp.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
 	envF := config.EnvironmentFetcher{}
 	port, err := envF.GetValue("PORT")
 	if err != nil {
