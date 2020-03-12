6104ef00978d05f3f671cb6712109a088a569583
MatiasBarrios <soymatiasbarrios@gmail.com>
2020-03-12 18:48:14 -0300

    master

--- main.go
+++ main.go
@@ -17,19 +17,19 @@ func main() {
 	defer f.Close()
 	log.SetOutput(f)
 	envF := config.EnvironmentFetcher{}
-	qcertificatekey, err := envF.GetValue("QCERTIFICATEKEY")
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	qcertificatecrt, err := envF.GetValue("QCERTIFICATECRT")
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
+	// qcertificatekey, err := envF.GetValue("QCERTIFICATEKEY")
+	// if err != nil {
+	// 	log.Fatalln(err.Error())
+	// }
+	// qcertificatecrt, err := envF.GetValue("QCERTIFICATECRT")
+	// if err != nil {
+	// 	log.Fatalln(err.Error())
+	// }
 	port, err := envF.GetValue("PORT")
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
 	log.Println("Starting app in port : ", port)
-	err = http.ListenAndServeTLS(":"+port, qcertificatecrt, qcertificatekey, router.GetRouter())
+	err = http.ListenAndServe(":"+port, router.GetRouter())
 	log.Println(err.Error())
 }
